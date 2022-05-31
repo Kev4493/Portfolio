026d8455245cd4f2cf492f512748ee1ae29f721a
Kevin Wagner <kevin_wagner4493@web.de>
2022-05-31 16:55:29 +0200

    finished  homesection and start with skills-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeSectionComponent } from './home-section/home-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeSectionComponent,
+    SkillsSectionComponent
   ],
   imports: [
     BrowserModule,
