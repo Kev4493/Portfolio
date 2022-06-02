13584dae79598a8daee645f71a49232b1bc6f497
Kevin Wagner <kevin_wagner4493@web.de>
2022-06-02 16:51:52 +0200

    added the portfolio-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeSectionComponent } from './home-section/home-section.component';
 import { SkillsSectionComponent } from './skills-section/skills-section.component';
+import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeSectionComponent,
-    SkillsSectionComponent
+    SkillsSectionComponent,
+    PortfolioSectionComponent
   ],
   imports: [
     BrowserModule,
