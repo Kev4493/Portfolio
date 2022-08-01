a8ef2654fc87ae9a85953d0ace8578556f865d90
Kevin Wagner <kevin_wagner4493@web.de>
2022-08-01 20:36:37 +0200

    added router link for Impressum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { MatIconModule } from '@angular/material/icon';
     PortfolioSectionComponent,
     AboutMeSectionComponent,
     ContactSectionComponent,
-    FooterSectionComponent
+    FooterSectionComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
