27b9723290671a00f6095b6acb38711dca1670e6
Kevin Wagner <kevin_wagner4493@web.de>
2022-08-02 17:17:14 +0200

    added responsive imprint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeSectionComponent } from './home-section/home-section.component';
 import { ImprintComponent } from './imprint/imprint.component';
 
 const routes: Routes = [
+  { path: '', component: HomeSectionComponent },
   { path: 'imprint', component: ImprintComponent }
 ];
 
