ee4f2b01504755a2304e1cc6c68bcdbcab10f677
Kevin Wagner <kevin_wagner4493@web.de>
2022-08-09 20:28:29 +0200

    changed button on home section

--- src/send_mail.php
+++ src/send_mail.php
@@ -39,8 +39,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case ("POST"): //Send the email;
         header("Access-Control-Allow-Origin: *");
 
-        $subject = "Contact From " . $_POST['name'];
-        $headers = "From:  noreply@developerakademie.com";
+        $subject = "Contact From " .$_POST['name'] . $_POST['email'];
+
+        // $subject = "Contact From" "echo $_POST['name']." ".$_POST['email'];.";
+        $headers = "From:  noreply@mywebsite.com";
 
         mail($recipient, $subject, $_POST['message'], $headers);
         header("Location: " . $redirect); 
